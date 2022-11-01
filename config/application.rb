d797a1f69c911807c3916b15a8ecdc24f82c4c7f
suzuki-kodai <kodai.pri1119@gmail.com>
2022-11-01 17:14:12 +0900

    ファイル作成制限の追加

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,6 @@ module RunteqNormal
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -29,5 +28,11 @@ module RunteqNormal
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
